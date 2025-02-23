10eadeec3b58eafa93fa6510be475246a261a9b9
eann1s <danilio.ponomarev7766@gmail.com>
2025-02-23 01:18:05 -0800

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!, {
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+      dbName: process.env.MONGO_INITDB_ROOT_DATABASE,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const swagger = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  Logger.log(
+    `Swagger is running on http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
+}
+bootstrap();
